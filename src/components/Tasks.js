147a7c785ff1158426458d3c5b6326d410171eac
Saurav Sharma <saurav.sharma.zz45@gmail.com>
2021-05-30 08:51:50 -0700

    Set up the project
    added some components - header, footer and configured them with App.js

---
+++ src/components/Tasks.js
@@ -0,0 +1,56 @@
+import { Card, Button, ButtonGroup } from 'react-bootstrap'
+
+function Tasks() {
+    return (
+        <Card>
+        <Card className="p-2">
+            <Card.Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus inventore excepturi blanditiis reiciendis, autem asperiores tempore.?</Card.Body>            
+            <Card.Body className="p-2 ">
+            <ButtonGroup aria-label="Basic">
+                <Button variant="warning">Edit</Button>
+                <Button variant="primary">Time</Button>
+                <Button variant="success">Done</Button>
+                <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+            </Card.Body>
+        </Card>
+        <Card className="p-2">
+            <Card.Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. uam numquam fuga pariatur? Architecto quidem, dolorum natus, porro maxime quam in, saepe aperiam quae asperiores tempore.?</Card.Body>            
+            <Card.Body className="p-2 ">
+            <ButtonGroup aria-label="Basic">
+                <Button variant="warning">Edit</Button>
+                <Button variant="primary">Time</Button>
+                <Button variant="success">Done</Button>
+                <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+            </Card.Body>
+        </Card>
+        <Card className="p-2">
+            <Card.Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus inventore excepturi blanditiis reiciendis, autem impedit enim quaerat nisi, quis explicabo quod adip dolorum natus, porro maxime quam in, saepe aperiam quae asperiores tempore.?</Card.Body>            
+            <Card.Body className="p-2 ">
+            <ButtonGroup aria-label="Basic">
+                <Button variant="warning">Edit</Button>
+                <Button variant="primary">Time</Button>
+                <Button variant="success">Done</Button>
+                <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+            </Card.Body>
+        </Card>
+        <Card className="p-2">
+            <Card.Body>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Ducimus inventore excepturi blanditiis reiciendis, autem impedi ores tempore.?</Card.Body>            
+            <Card.Body className="p-2 ">
+            <ButtonGroup aria-label="Basic">
+                <Button variant="warning">Edit</Button>
+                <Button variant="primary">Time</Button>
+                <Button variant="success">Done</Button>
+                <Button variant="danger">Remove</Button>
+            </ButtonGroup>
+            </Card.Body>
+        </Card>
+
+        
+        </Card>
+    )
+}
+
+export default Tasks
